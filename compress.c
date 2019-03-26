ef4c3b280c5c7c99e820b194d917b907f721c2dc
Ashvi Shah <ashvi23@gmail.com>
2019-03-26 12:12:31 -0400

    compress untested done

--- compress.c
+++ compress.c
@@ -12,18 +12,53 @@ void tokenize(char* pathName);
 
 void compress(const char* tocompress, const char* codebook){
     /*
-    open tocompress 
+    open tocompress //its a path
     open codebook
 
     read the file to compress, tokenize it
     send the token and codebook to a method that finds the string and returns its huffman code
     take this code and append it to the compressedfile
     */
-    FILE* tc = fopen(tocompress);
+    //FILE* tc = fopen(tocompress);
+    int fd= open(tocompress, O_RDONLY);
+    int size=lseek(fd, 0, SEEK_END);//find the size of the file
+    char* buffer= NULL;
+    buffer=(char*) malloc(sizeof(char)*size);
+    int bufIndex=0;
+    //create the output file
+    FILE* towrite = fopen("tester.txt" , "ab+");
+  
+    while(read(fd, buffer,1)){
+        //check for error
+        if(buffer[bufIndex]=='\n' || buffer[bufIndex]=='\t' || buffer[bufIndex]=='\r' || buffer[bufIndex]== '\v' || buffer[bufIndex]==' '){
+            char* delim=(char*) malloc(sizeof(char)*2);
+            delim[0]=buffer[buffIndex];
+            delim[1]='\0';
+            buffer[bufIndex]='\0';
+            char* currNodeName= (char*) malloc(sizeof(char)*bufIndex+1);
+            strncpy(currNodeName, buffer, buffIndex+1);
+            int count =0;
+            for(i=0; i<buffIndex+1; i++){//getting the accumulated ascii value of the token
+                sum+=currNodeName[i];
+                count++;
+            }
+            char* hcode;
+            hcode = retcode(currNodeName, codebook); 
+             if(towrite == NULL){
+                printf("unable to create file\n");
+                return;
+            }
+            else{
+               fputs(hcode, towrite); 
+            }
 
+        }
+        bufIndex++;
+    }    
+fclose(towrite);
+close(fd);
 
 }
-
 
 char* retcode(char* tofind, const char* codebook){
     
@@ -72,10 +107,7 @@ void parseFile(char* pathName){
             for(i=0; i<buffIndex+1; i++){
                 sum+=currNodeName[];
             }
-            struct AVLNode* newNode=makeAVLNode(, char* string)
-            //copy val to binary tree node
-            //call fxn to add node to tree
-            //add delimeter to tree as well
+            
         }
         bufIndex++;
     }

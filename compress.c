46488c1b1116dd6085dd36fb127ecc9828f758e1
Ashvi Shah <ashvi23@gmail.com>
2019-03-26 12:13:16 -0400

    compress untested done/ delete extra code

--- compress.c
+++ compress.c
@@ -87,32 +87,3 @@ char* retcode(char* tofind, const char* codebook){
 
 close(codebook);
 }
-
-
-
-void parseFile(char* pathName){
-    int fd= open(pathName, O_RDONLY);
-    int size=lseek(fd, 0, SEEK_END);//find the size of the file
-    char* buffer= NULL;
-    buffer=(char*) malloc(sizeof(char)*size);
-    int bufIndex=0;
-    while(read(fd, buffer,1)){
-        //check for error
-        if(buffer[bufIndex]=='\n' || buffer[bufIndex]=='\t' || buffer[bufIndex]=='\r' || buffer[bufIndex]== '\v' || buffer[bufIndex]==' '){
-            buffer[bufIndex]='\0';
-            char* currNodeName= (char*) malloc(sizeof(char)*bufIndex+1);
-            strncpy(currNodeName, buffer, buffIndex+1);
-            int i=0;
-            int sum=0;
-            for(i=0; i<buffIndex+1; i++){
-                sum+=currNodeName[];
-            }
-            
-        }
-        bufIndex++;
-    }
-    
-    return;
-}
-
-

88d3f0f740d9c7c2be2abb80eb2e095566e4a72f
esmartens <47702686+esmartens@users.noreply.github.com>
2019-03-31 16:53:55 -0400

    possibly all done????

--- buildCBook.c
+++ buildCBook.c
@@ -2,39 +2,71 @@
 #include <stdlib.h>
 #include <unistd.h> 
 #include <sys/types.h> 
-void parseFile(char* pathName)
-
-void parseFile(char* pathName){
-	int fd= open(pathName, O_RDONLY);
-	int size=lseek(fd, 0, SEEK_END);//find the size of the file
-	char* buffer= NULL;
-	buffer=(char*) malloc(sizeof(char)*size);
-	int bufIndex=0;
-	while(read(fd, buffer,1)){
-		//check for error
-		if(buffer[bufIndex]=='\n' || buffer[bufIndex]=='\t' || buffer[bufIndex]=='\r' || buffer[bufIndex]== '\v' || buffer[bufIndex]==' '){
-			char* delim=(char*) malloc(sizeof(char)*2);
-			delim[0]=buffer[buffIndex];
-			delim[1]='\0';
-			buffer[bufIndex]='\0';
-			char* currNodeName= (char*) malloc(sizeof(char)*bufIndex+1);
-			strncpy(currNodeName, buffer, buffIndex+1);
-			int i=0;
-			int sum=0;
-			int delimSum+=delim[0];
-			for(i=0; i<buffIndex+1; i++){//getting the accumulated ascii value of the token
-				sum+=currNodeName[i];
-			}
-			struct AVLNode* newNode=makeAVLNode(sum, currNodeName);
-			struct AVLNode* delimNode=makeAVLNode(delimSum, delim);
-			structAVLNode* tree=insert(newNode, sum);
-			tree=insert(delimNode, delimSum);//insert delimeter node
-			//copy val to binary tree node
-			//call fxn to add node to tree
-			//add delimeter to tree as well
+#include <fcntl.h>
+void buildCBook.c(struct AVLNode* hufftree, const char* pathname);
+void traverseTree(struct AVLNode* root, int filedesc,  int dir);//dir is direction
+void buildCBook.c(struct AVLNode* hufftree, const char* pathname){
+		int fd=open("./HuffmanCodebook", O_WRONLY | O_CREAT, 00644);
+		//traverse through Huffman tree (inorder), printing each time a leaf is found(i.e. leftchild/rightchild ==null or ig if name != NULL), print on each traversal downward.
+		if(write(fd, "^%\n", 3)!=3){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+			} 
+		traverseTree(hufftree, fd, -1);
+		int success=close(fd);
+		if(success!=0){
+		printf("problem closing file descriptor\n");
 		}
-		bufIndex++;
+		return;
 	}
-	
-	return;
-}
+
+void traverseTree(struct AVLNode* root, int filedesc, int dir){
+		if(dir==0){
+			//print 0 to file
+			if(write(fd, "0", 1)!=1){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+			}
+		}
+		else{
+			//print 1 to file
+			if(write(fd, "1", 1)!=1){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+			}
+		}
+		if(root->left == NULL && root->right==NULL){
+			//when you get to the leaf node, print a space and then the name contained in that node
+			write(fd, "\t",1);
+			int currTokLen=strlen(root->name);
+			if(strncmp(root->name, '\n')==0){//writing escape characters to file
+				if(write(fd, "^%\\n", currTokLen+2)!=(currTokLen+2)){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+				}
+			}
+			else if(strncmp(root->name, '\t')==0 ){
+				if(write(fd, "\\t", currTokLen+2)!=currTokLen+2){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+				}
+			} 
+			else if(strncmp(root->name, '\r'){
+				if(write(fd, "\\r", currTokLen+2)!=currTokLen+2){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+				}
+			}
+			else if(strncmp(root->name, '\v')==0){
+				if(write(fd, "\\v", currTokLen+2)!=currTokLen+2){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+				}
+			}
+			else if(strncmp(root->name, '\')==0 ){
+				if(write(fd, "\\", currTokLen+2)!=currTokLen+2){
+				write(2,"There was an error writing to HuffmanCodebook\n", 47);
+				}
+			}
+			else{
+				write(fd, root->name, currTokLen);
+				}
+			write(fd, "\n", 1);//write newline
+		}
+		traverseTree(root->left,fd, 0);
+		traverseTree(root->right,fd, 1);
+		return;
+	}

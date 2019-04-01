3801b1160eaae5ca30a25d1a920307c7583dace6
ashvi23 <36307728+ashvi23@users.noreply.github.com>
2019-04-01 10:34:12 -0400

    changed realloc! works better now!

---
+++ recentVersions/getNextToken.c
@@ -0,0 +1,56 @@
+har* getNextToken(char* filename, int size, int offset){
+    if(size ==0){
+        return "3";
+    }
+    int fd= open(filename, O_RDONLY);
+    int l= lseek(fd, offset, SEEK_SET);
+    char* token= NULL;
+    token = (char*)malloc(10*sizeof(char));
+    if(token == NULL){
+    printf("Malloc failed to get next token\n");
+    return "3";
+    }
+    char* tempchar = (char*) malloc(2 * sizeof(char));
+    if(tempchar == NULL){
+    printf("Malloc failed in get next token\n");
+    return "3";
+    }
+    int tokcounter =0;
+    token[0] = '\0';
+    int tokensize = sizeof(token);
+    while(read(fd, tempchar,1 )){
+        if(sizeof(token)-strlen(token) >=2){
+        strcat(token, tempchar);
+       }
+       else{
+       	tokensize = sizeof(token);
+        printf("size of token:%d\n", tokensize);
+        char* temp = (char*) realloc(token , (tokensize+10) * sizeof(char) );
+        if(temp ==NULL){
+            printf("Error: Cannot malloc space for token\n");
+            return "3";
+        }
+        else{
+            token = temp;
+            strcat(token, tempchar);
+        }
+        
+       }
+        
+int tempcharint = tempchar[0];
+        if( tempchar[0]==' '|| iscntrl( tempchar[0])>0) {
+            close(fd);
+            return token; 
+            break;   
+        }
+        if(tokcounter+1 == size ){
+           free(tempchar);
+            close(fd);
+            return token;
+        }
+        tokcounter++;
+    }
+free(token);
+return "3";
+
+}

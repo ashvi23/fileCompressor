0a77cdb7c20c455769c4e7a1b8bf0a59056bc0dc
esmartens <47702686+esmartens@users.noreply.github.com>
2019-04-03 01:12:14 -0400

    I think this is the latest .h

--- fileCompressor.h
+++ fileCompressor.h
@@ -9,8 +9,32 @@
 #include <sys/stat.h>
 #include <ctype.h>
 
+struct HashNode{
+	char* token;
+	int key;
+	int frequency;
+	struct HashNode* next;
+};
+struct HeapNode{
+	int frequency;
+	char* name;//token gathered
+	int height;
+	struct HeapNode* left;
+	struct HeapNode* right;
+};
+struct LLNode{
+	struct HeapNode* Tree;
+	struct LLNode* next;
+	};
+
+
+
+struct HashNode* HashTable[10000];
+//main
+void error(int err);
+void deallocate(struct HashNode* head);
 //dirwalk
-void listdir(const char* dirname);
+void listdir(int flag, const char* dirname, const char* codebook);
 void openfiles(const char* filename);
 int is_directory(const char *dir);
 void writefiles(const char* filename);
@@ -22,7 +46,7 @@ char* retcode(char* tofind, char* codebook);
 int isFile(char *to_read) ;
 
 //decompress
-void decompress(char* codebook, char* decompressed)
+void decompress(char* codebook, char* decompressed);
 char* itoa(int num, char* str, int base) ;
 
 //treefromcbook
@@ -43,7 +67,8 @@ struct LLNode* makeNode(struct LLNode* newNode, struct HeapNode* tree);
 struct HeapNode* makeHeapNode(struct HeapNode* node, int freq, char* token);
 void delete(struct LLNode **head, char* Name);
 void insert(struct LLNode **head, struct HeapNode **tree, int freq);
-void freeNodes(struct LLNode *head);
+//void freeNodes(struct LLNode *head);
+void freeTree(struct HeapNode* headTree);
 void printTree(struct HeapNode* node);
 void printLL(struct LLNode *head);
 
@@ -51,3 +76,4 @@ void printLL(struct LLNode *head);
 void swap(struct HeapNode** arr, int size, int largest);
 void heapify(struct HeapNode** arr, int size, int i);
 struct HeapNode* hashToArr(struct HashNode** HashTable, int numToks);
+
